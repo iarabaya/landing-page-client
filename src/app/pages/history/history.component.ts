05e61bd92ad552c671636098f577b249776ba02c
iara baya <iarabayav@gmail.com>
2021-12-02 14:51:45 -0300

    records page finished, change update button for a fab button, pipes added for table data

--- src/app/pages/history/history.component.ts
+++ src/app/pages/history/history.component.ts
@@ -4,8 +4,8 @@ export interface RecordItem {
   fecha: string;
   marca: string;
   modelo: string;
-  equipo: number;
-  envio: number;
+  equipo: string;
+  envio: string;
   entregado: string;
 }
 
@@ -18,13 +18,58 @@ export class HistoryComponent {
   displayedColumns: string[] = ['fecha', 'marca', 'modelo', 'equipo','envio', 'entregado'];
   dataSource: RecordItem[] = [];
 
+  envioMap = {
+    '1' : 'retiro pendiente',
+    '2' : 'retiro asignado',
+    '3' : 'retirado',
+    '4' : 'reparacion pendiente',
+    '5' : 'reparado',
+    '6' : 'entrega asignada',
+    '7' : 'entrega pendiente',
+    '8' : 'entregado',
+    '9' : 'recibido',
+    '10' : 'renunciado',
+  }
+
+  estadoMap = {
+    '1' : 'a reparar',
+    '2' : 'a reparar',
+    '3' : 'a reparar',
+    '4' : 'a reparar',
+    '5' : 'reparado',
+    '6' : 'reparado',
+    '7' : 'reparado',
+    '8' : 'reparado',
+    '9' : 'reparado',
+    '10' : 'renunciado',
+  }
+
   constructor(private equipmentService: EquipmentService){}
 
   getRecords(){
     const clientId = parseInt(localStorage.getItem('clientId') || '1');
 
-    this.equipmentService.getEquipment(clientId).subscribe(res => {
-      console.log(res);
-    })
+    const parseDate = (date: string) => {
+      const newDate = new Date(date);
+      return newDate.toLocaleString('es-ES', {month:'numeric', day:'numeric'})
+    }
+
+      this.equipmentService.getEquipment(clientId).subscribe(
+        res => {
+          console.log(res);
+          res.forEach(e => {
+            const item: RecordItem = {
+                fecha: parseDate(e.travelEquipmentDTOs[0].operationDate),
+                marca: e.mark || '',
+                modelo: e.model || '',
+                equipo:  e.travelEquipmentDTOs.length > 1? e.travelEquipmentDTOs[1].statusTravel.toString() : e.travelEquipmentDTOs[0].statusTravel.toString(),
+                envio: e.travelEquipmentDTOs.length > 1? e.travelEquipmentDTOs[1].statusTravel.toString() : e.travelEquipmentDTOs[0].statusTravel.toString(),
+                entregado: e.travelEquipmentDTOs.length > 1? e.travelEquipmentDTOs[1].operationDate : ''
+              }
+            this.dataSource.push(item)
+          });
+        }
+      )
   }
 }
+
--- src/app/pages/status/status.component.ts
+++ src/app/pages/status/status.component.ts
@@ -1,3 +1,4 @@
+import { I18nSelectPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { EquipmentService } from '../../services/equipment/equipment.service';
 
@@ -5,8 +6,8 @@ export interface EquipmentItem {
   fecha: string;
   marca: string;
   modelo: string;
-  equipo: number;
-  envio: number;
+  equipo: string;
+  envio: string;
 }
 
 @Component({
@@ -18,6 +19,32 @@ export class StatusComponent {
   displayedColumns: string[] = ['fecha', 'marca', 'modelo', 'equipo','envio'];
   dataSource : EquipmentItem[] = [];
 
+  envioMap = {
+    '1' : 'retiro pendiente',
+    '2' : 'retiro asignado',
+    '3' : 'retirado',
+    '4' : 'reparacion pendiente',
+    '5' : 'reparado',
+    '6' : 'entrega asignada',
+    '7' : 'entrega pendiente',
+    '8' : 'entregado',
+    '9' : 'recibido',
+    '10' : 'renunciado',
+  }
+
+  estadoMap = {
+    '1' : 'a reparar',
+    '2' : 'a reparar',
+    '3' : 'a reparar',
+    '4' : 'a reparar',
+    '5' : 'reparado',
+    '6' : 'reparado',
+    '7' : 'reparado',
+    '8' : 'reparado',
+    '9' : 'reparado',
+    '10' : 'renunciado',
+  }
+
   constructor(private equipmentService: EquipmentService){}
 
   getStatus(){
@@ -28,6 +55,7 @@ export class StatusComponent {
       return newDate.toLocaleString('es-ES', {month:'numeric', day:'numeric'})
     }
 
+    
     this.equipmentService.getEquipment(clientId).subscribe(
       res => {
         console.log(res)
@@ -36,8 +64,8 @@ export class StatusComponent {
             fecha: parseDate(e.travelEquipmentDTOs[0].operationDate),
             marca: e.mark || '',
             modelo: e.model || '',
-            equipo: e.travelEquipmentDTOs.length > 1? e.travelEquipmentDTOs[1].statusTravel : e.travelEquipmentDTOs[0].statusTravel ,
-            envio: e.travelEquipmentDTOs.length > 1? e.travelEquipmentDTOs[1].statusTravel : e.travelEquipmentDTOs[0].statusTravel
+            equipo:  e.travelEquipmentDTOs.length > 1? e.travelEquipmentDTOs[1].statusTravel.toString() : e.travelEquipmentDTOs[0].statusTravel.toString(),
+            envio: e.travelEquipmentDTOs.length > 1? e.travelEquipmentDTOs[1].statusTravel.toString() : e.travelEquipmentDTOs[0].statusTravel.toString()
           }
           this.dataSource.push(item)
         });
